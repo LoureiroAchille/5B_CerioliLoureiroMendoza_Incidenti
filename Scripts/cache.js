794c069a57f4923ea6c749e4cdeea9e76ec27f7e
Marco <ceriolimarco@itis-molinari.eu>
2024-11-15 11:40:47 +0100

    cache

--- Scripts/cache.js
+++ Scripts/cache.js
@@ -20,28 +20,28 @@ const upload = (data) => {
         }
       })    
     })
-  }
-  
-  const download = () => {
+}
+
+const download = () => {
     return new Promise((resolve,reject)=>{
-      fetch("conf.json").then(r => r.json()).then(confData => {
-        try{
-          fetch(confData.url + "get", {
-            method: "POST",
-            headers: {
-              'Content-Type': 'application/json',
-              'key': confData.token
-            },
-            body: JSON.stringify({
-              key: confData.key
-            })
-          }).then(r => r.json())
-          .then(data => {resolve(data.result);})
-        }catch(error) {
-          reject(error)
-        }
-      })
+        fetch("conf.json").then(r => r.json()).then(confData => {
+            try{
+                fetch(confData.url + "get", {
+                    method: "POST",
+                    headers: {
+                    'Content-Type': 'application/json',
+                    'key': confData.token
+                    },
+                    body: JSON.stringify({
+                    key: confData.key
+                    })
+                }).then(r => r.json())
+                .then(data => {resolve(data.result);})
+            }catch(error) {
+                reject(error)
+            }
+        })
     })   
-  }
-  
-  export{upload,download};
+}
+
+export{upload,download};
