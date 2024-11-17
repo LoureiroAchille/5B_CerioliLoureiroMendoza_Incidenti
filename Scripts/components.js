216dc14e961ec76e79f7a3bfdb2b308e221b6c83
Mendoza Enzo <mendozawayne@itis-molinari.eu>
2024-11-17 20:04:31 +0100

    aggiunta tabella e creazione modale

--- Scripts/components.js
+++ Scripts/components.js
@@ -1,66 +1,65 @@
 import{upload,download} from "./cache.js";
 
-const createForm=(elem)=>{
-    let data;
-    let element=elem;
-    let callback;
-    return{
-        setLabels: (labels) => { data = labels; },
-        setCallback: (f) => { callback = f; },
-        render: () => { 
-            element.innerHTML=data.map((line)=>`<div>${line[0]}<input id="${line[0]}" type="${line[1]}"></div>`).join("");
-            element.innerHTML += `<button type="button" id="invia"> Invia </button>`;  
-            document.getElementById("invia").onclick = () => {
-                const result = data.map((name) => {return document.getElementById(name[0]).value});
-                callback(result).then((object) => {
-                    console.log(object);
-                    download().then((places) => {
-                        console.log(places);
-                        places.push(object);
-                        upload(places).then(renderMap);
-                        
-                    })
-                });
-            }
-        },        
-    };
+const createForm = (elem) => {
+  let data;
+  let element = elem;
+  let callback;
+
+  return {
+      setLabels: (labels) => { data = labels; },
+      setCallback: (f) => { callback = f; },
+      render: () => {
+          element.innerHTML = data.map((line) => 
+              `<div>${line[0]}<input id="${line[0]}" type="${line[1]}"></div>`
+          ).join('');
+          element.innerHTML += `<button type="button" id="invia">Invia</button>`;
+          
+          document.getElementById("invia").onclick = () => {
+              const result = data.map((name) => {
+                  return document.getElementById(name[0]).value;
+              });
+
+              // Chiamata al callback per ottenere le coordinate 
+              callback(result).then((object) => {
+                  console.log(object);
+
+                  
+                  download().then((places) => {
+                      places.push(object); // Aggiungiunta incidente
+                      upload(places).then(() => {
+                          alert("Incidente aggiunto con successo!");
+                          renderMap(); 
+                          element.innerHTML = ''; // Pulisce il form
+                      });
+                  });
+              });
+          };
+      },
+  };
 };
 
-let zoom = 12;
-let maxZoom = 19;
-let map = L.map('map').setView([45.4654219,9.1859243], zoom);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: maxZoom,
-attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
 
 const renderMap = () => {
-    /*upload([
-   {
-      name: "Piazza del Duomo",
-      coords: [45.4639102, 9.1906426],
-   },
-   {
-      name: "Darsena",
-      coords: [45.4536286, 9.1755852]
-   },
-   {
-      name: "Parco Lambro",
-      coords: [45.4968296, 9.2505173]
-   },
-   {
-      name: "Stazione Centrale",
-      coords: [45.48760768, 9.2038215]
-   }
-]);*/
-    download().then((places) => {
-        places.forEach((place) => {
-            console.log(place);
-            const marker = L.marker(place.coords).addTo(map);
-            marker.bindPopup(`<b>${place.name}</b><b>${place.name}</b><b>${place.name}</b>`);
-        });
-    })
-}
+  download().then((places) => {
+      const map = L.map('map').setView([45.4654219, 9.1859243], 12);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
+
+      places.forEach((place) => {
+          const marker = L.marker(place.coords).addTo(map);
+          marker.bindPopup(`
+              <b>${place.name}</b><br>
+              <b>Data e Ora:</b> ${place.date}<br>
+              <b>Feriti:</b> ${place.injured}<br>
+              <b>Morti:</b> ${place.dead}
+          `);
+      });
+  });
+};
+
+
 
 function createTable() {
     const container = document.getElementById('form');
@@ -115,8 +114,48 @@ function createTable() {
       };
     });
   }
+  const handleAddIncident = () => {
+    const formContainer = document.getElementById('form');
+
+    const formHtml = `
+        <div id="incidentForm">
+            <h3>Inserisci Nuovo Incidente</h3>
+            <div>Indirizzo: <input id="address" type="text"></div><br>
+            <div>Targhe Coinvolte: <input id="plates" type="text"></div><br>
+            <div>Data e Ora: <input id="dateTime" type="datetime-local"></div><br>
+            <div>Feriti: <input id="injured" type="number"></div><br>
+            <div>Morti: <input id="dead" type="number"></label><br>
+            <button id="saveIncidentButton">Salva Incidente</button>
+        </div>
+    `;
+
+    formContainer.innerHTML = formHtml;
+
+    document.getElementById('saveIncidentButton').onclick = () => {
+        const newIncident = {
+            name: document.getElementById('address').value,
+            plates: document.getElementById('plates').value.split(','),
+            date: document.getElementById('dateTime').value,
+            injured: parseInt(document.getElementById('injured').value),
+            dead: parseInt(document.getElementById('dead').value),
+            coords: [45.4654219, 9.1859243], // Coordinate di default (Milano)
+        };
+
+        download().then((places) => {
+            places.push(newIncident);
+            upload(places).then(() => {
+                renderMap();
+                alert("Incidente aggiunto con successo!");
+                formContainer.innerHTML = ''; // Pulisce il form
+            });
+        });
+    };
+};
+
+
+// Esporta la funzione per essere utilizzata in script.js
 
 
 
 
-export{createForm,renderMap,createTable};
+export{createForm,renderMap,createTable,handleAddIncident};
